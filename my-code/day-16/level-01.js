02d4926ddb96d672714090a083dbe3688f299fc2
Janitha Manatunga <janithamanatunga@gmail.com>
2026-07-14 22:17:17 +0530

    complete day 16 exercises

---
+++ my-code/day-16/level-01.js
@@ -0,0 +1,11 @@
+const skills = ['HTML', 'CSS', 'JS', 'React','Node', 'Python'];
+const skillsTxt = JSON.stringify(skills, undefined, 4);
+console.log(skillsTxt);
+
+let age = 250;
+const ageTxt = JSON.stringify(age, undefined, 4);
+console.log(ageTxt);
+
+let isMarried = true;
+const isMarriedTxt = JSON.stringify(isMarried, undefined, 4);
+console.log(isMarriedTxt);
